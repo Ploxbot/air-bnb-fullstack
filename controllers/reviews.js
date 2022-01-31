675c7be9c40de828c39f18bc521deb5eb84305e8
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-01-31 15:53:41 +0700

    deleted basic controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,10 +5,6 @@ const router = express.Router()
 
 // Views
 
-// Basic Controllers
-router.get('/', (req, res) => {
-	res.send('reviews-page')
-})
 // Nested Controllers
 router.post('/', (req, res) => {
 	res.send('profil')
